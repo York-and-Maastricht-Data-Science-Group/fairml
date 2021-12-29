421f5bd69cca9c87df0efc2bc4f752147a85a57f
alfa-yohannis <62646509+alfayohannis@users.noreply.github.com>
2021-12-29 22:34:24 +0000

    Update Generator
    Update Generator

--- examples/org.eclipse.epsilon.examples.fairml.decisiontree/backup/decision_tree.py
+++ examples/org.eclipse.epsilon.examples.fairml.decisiontree/backup/decision_tree.py
@@ -1,8 +1,14 @@
 # load machine learning libraries
 import pandas as pd
-from sklearn.tree import * 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import * 
-from sklearn import *
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 # load AI Fair 360 libraries
 from aif360.metrics import *
@@ -20,19 +26,19 @@ print("========================")
 print("FairML: Decision Tree")
 print("========================")
 print("Description:")
-print("Testing Decision Tree for FairML")
+print("Predict whether income exceeds $50K/yr based on census data. Also known as \"Census Income\" dataset. <=50K = 0, >50K = 1 female = 0, male = 1 other = 0, white = 1")
 
 print("")
 print("========================")
-print("Bias Mitigation: Age Bias Mitigation")
+print("Bias Mitigation: Sex Bias Checking with Reweighing")
 print("------------------------")
-resource = "data/diabetes.csv"
-protected_attributes = ['young']
-predicted_attribute = 'label'
-dropped_attributes = ['age']
-test_size = 7
+resource = "data/adult.data.csv"
+protected_attributes = ['sex']
+predicted_attribute = 'class'
+dropped_attributes = ['race']
+training_size = 7
 validation_size = 0
-training_size = 3
+test_size = 3
 total_size = training_size + validation_size + test_size
 priviledgedGroup = 1 
 unprivilegedGroup = 0
@@ -46,7 +52,7 @@ dataset_original = FairMLDataset(df=data, label_name=predicted_attribute, favora
                 instance_weights_name=None,
                 # categorical_features=feature_cols,
                 features_to_keep=[],
-                features_to_drop=[],
+                features_to_drop=dropped_attributes,
                 na_values=[], 
                 custom_preprocessing=None,
                 metadata=None)
@@ -57,15 +63,16 @@ privileged_groups = [{protected_attributes[0] : priviledgedGroup}]
 unprivileged_groups = [{protected_attributes[0] : unprivilegedGroup}]
 
 print("")
-print("Original Bias Checking: Age Bias Checking")
+print("Original Bias Checking: Sex Bias Checking")
 print("-------------")
-print("Dataset: Pima Indians Diabetes Database")
+print("Dataset: Adult Census Income")
 metric_original_train = BinaryLabelDatasetMetric(dataset_original_train, 
                                              unprivileged_groups=unprivileged_groups,
                                              privileged_groups=privileged_groups)
 explainer_train = MetricTextExplainer(metric_original_train)
 			
 print("Original explainer: " + explainer_train.disparate_impact())
+print("Original statistical_parity_difference: %f" % metric_original_train.statistical_parity_difference())
 print("Original mean_difference: %f" % metric_original_train.mean_difference())
 
 
@@ -134,34 +141,31 @@ mitigation_method = Reweighing(unprivileged_groups=unprivileged_groups,
 dataset_mitigated_train = mitigation_method.fit_transform(dataset_original_train)
 
 print("")
-print("After Mitigation Bias Checking: Age Bias Checking")
+print("After Mitigation Bias Checking: Sex Bias Checking")
 print("-------------")
-print("Dataset: Pima Indians Diabetes Database")
+print("Dataset: Adult Census Income")
 metric_mitigated_train = BinaryLabelDatasetMetric(dataset_mitigated_train, 
                                              unprivileged_groups=unprivileged_groups,
                                              privileged_groups=privileged_groups)
 explainer_train = MetricTextExplainer(metric_mitigated_train)
 			
 print("After mitigation explainer: " + explainer_train.disparate_impact())
+print("After mitigation statistical_parity_difference: %f" % metric_mitigated_train.statistical_parity_difference())
 print("After mitigation mean_difference: %f" % metric_mitigated_train.mean_difference())
 	
-#split dataset in features and target variable
-attributes = list(dataset_mitigated_train.df.columns)
-attributes.remove(predicted_attribute)
-for a in dropped_attributes:
-    attributes.remove(a)
-X = dataset_mitigated_train.df[attributes] # Features
-y = dataset_mitigated_train.df[predicted_attribute] # Target variable
 
-# Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size / total_size, random_state=1)  
 print("")
 print("After Mitigation Training: DecisionTreeClassifier, Parameters: ")
 print("-------------")
 
 classifier = DecisionTreeClassifier()
-classifier = classifier.fit(X_train,y_train)
-y_pred = classifier.predict(X_test)
+model = make_pipeline(StandardScaler(),
+                      classifier)
+fit_params = {'decisiontreeclassifier__sample_weight': dataset_mitigated_train.instance_weights}
+model_mitigated_train = model.fit(dataset_mitigated_train.features, dataset_mitigated_train.labels.ravel(), **fit_params)
+
+y_pred = model_mitigated_train.predict(dataset_original_test.features)
+y_test = dataset_original_test.labels
 
 # Model Accuracy, how often is the classifier correct?
 after_mitigation_accuracy = metrics.accuracy_score(y_test, y_pred)
@@ -173,13 +177,19 @@ tree.plot_tree(classifier,
                filled=True,
                rounded=True);
 plt.savefig('graphics/Mitigated-DecisionTreeClassifier_.png')
+
 print("")
 print("After Mitigation Training: DecisionTreeClassifier, Parameters: criterion='gini', max_depth=3")
 print("-------------")
 
 classifier = DecisionTreeClassifier(criterion='gini', max_depth=3)
-classifier = classifier.fit(X_train,y_train)
-y_pred = classifier.predict(X_test)
+model = make_pipeline(StandardScaler(),
+                      classifier)
+fit_params = {'decisiontreeclassifier__sample_weight': dataset_mitigated_train.instance_weights}
+model_mitigated_train = model.fit(dataset_mitigated_train.features, dataset_mitigated_train.labels.ravel(), **fit_params)
+
+y_pred = model_mitigated_train.predict(dataset_original_test.features)
+y_test = dataset_original_test.labels
 
 # Model Accuracy, how often is the classifier correct?
 after_mitigation_accuracy = metrics.accuracy_score(y_test, y_pred)
@@ -191,19 +201,4 @@ tree.plot_tree(classifier,
                filled=True,
                rounded=True);
 plt.savefig('graphics/Mitigated-DecisionTreeClassifier_criterion=gini-max_depth=3.png')
-
-
-	
-# print("")
-# print("-------------")
-# print("After Mitigation Bias Checking: Age Bias Checking")
-# print("-------------")
-# metric_mitigated_train = BinaryLabelDatasetMetric(dataset_mitigated_train, 
-#                                                unprivileged_groups=unprivileged_groups,
-#                                                privileged_groups=privileged_groups)
-
-# explainer_train = MetricTextExplainer(metric_mitigated_train)
-# print(explainer_train.disparate_impact())
-# print("Difference in mean outcomes between unprivileged and privileged groups = %f" % metric_mitigated_train.mean_difference())
-
 
